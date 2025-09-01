cc5d5f0baf03adc55afbcda9a7564258b1634a7b
Rafael Duarte de Medeiros <rafael.medeiros@amlabs.com.br>
2025-09-01 19:29:28 -0300

    Add new script for processing text with translation and summarization using LangChain and Google Generative AI

---
+++ 2-chains-e-processos/4-pipeline-de-processamento.py
@@ -0,0 +1,23 @@
+from langchain.prompts import PromptTemplate
+from langchain_google_genai import ChatGoogleGenerativeAI
+from langchain_core.output_parsers import StrOutputParser
+from dotenv import load_dotenv
+load_dotenv()
+
+template_translate = PromptTemplate(
+    input_variables=["initial_text"],
+    template="Translate the following text to English:\n ```{initial_text}```"
+)
+
+template_summarize = PromptTemplate(
+    input_variables=["text"],
+    template="Summarize the following text in 4 words:\n ```{text}```"
+)
+
+llm_en = ChatGoogleGenerativeAI(model="gemini-2.5-pro", temperature=0)
+
+translate = template_translate | llm_en | StrOutputParser()
+pipeline = {"text": translate} | template_summarize | llm_en | StrOutputParser()
+
+response = pipeline.invoke({"initial_text": "Bolsonaro vai ser preso e vamos dar muita festa!"})
+print(response)
